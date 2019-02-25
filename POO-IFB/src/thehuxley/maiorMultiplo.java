287d93939f639b360e11f97dfa07fba2989a33d6
marcosvandrade <Aluno@admin-PC>
2019-02-25 15:13:44 -0300

    atualizacao maiorMultiplo

--- POO-IFB/src/thehuxley/maiorMultiplo.java
+++ POO-IFB/src/thehuxley/maiorMultiplo.java
@@ -12,23 +12,35 @@ public class maiorMultiplo {
 		int N = s.nextInt();
 		
 		int maior = 0;
+		int cont = 0;
+		int i;
 		
-			for(int i = M; i <= N; i = i + i) {
+		if((M > N) || (N == 0) || (M == 0)) {
+			
+			System.out.println("sem multiplos menores que " + N);
+			
+		} else {
+		
+			for(i = 1; i <= N; i++) {
 				
-				maior = maior + i;
+				if(i % M == 0) {
+					cont = cont + 1;
+					if(i >= maior) {
+						maior = i;
+					}
+					if(cont == 0) {
+						System.out.println("sem multiplos menores que " + N);
+					}
+				}
 				
 			} 
 			
-			if(maior == 0) {
-				
-				System.out.println("sem multiplos menores que " + N);
-				
-			} else {
-				
+		}
+		
+		if(cont != 0) {
 			System.out.println(maior);
-			
-			  } 
-			
+		}
+		
 		s.close();
 		
 	}
