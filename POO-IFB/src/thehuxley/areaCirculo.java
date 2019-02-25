9a6d10f5a8b40ae2a9ac5eaa1256df61c42e825e
marcosvandrade <Aluno@admin-PC>
2019-02-25 15:36:47 -0300

    areaCirculo

---
+++ POO-IFB/src/thehuxley/areaCirculo.java
@@ -0,0 +1,16 @@
+package thehuxley;
+
+import java.util.Scanner;
+
+public class areaCirculo {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		float raio = s.nextFloat();
+		
+		 pi = 3.14159;
+		
+	}
+
+}
