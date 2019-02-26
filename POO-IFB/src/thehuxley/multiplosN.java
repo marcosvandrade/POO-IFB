72bd5320ed2fd4c82d5830e0ed082b4450f94be3
Marcos Vasconcellos de Andrade <dcemvasconcellos@DCEM-02182.dgp.local>
2019-02-26 16:54:24 -0300

    multiplos de N entre os valores A e B.

---
+++ POO-IFB/src/thehuxley/multiplosN.java
@@ -0,0 +1,49 @@
+package thehuxley;
+
+import java.util.Scanner;
+
+public class multiplosN {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		int M = s.nextInt();
+		int N = s.nextInt();
+		
+		int maior = 0;
+		int cont = 0;
+		int i;
+		
+		if((M > N) || (N == 0) || (M == 0)) {
+			
+			System.out.println("sem multiplos menores que " + N);
+			
+		} else {
+		
+			for(i = 1; i <= N; i++) {
+				
+				if(i % M == 0) {
+					cont = cont + 1;
+					if(i >= maior) {
+						maior = i;
+					}
+					if(cont == 0) {
+						System.out.println("sem multiplos menores que " + N);
+					}
+				}
+				
+			} 
+			
+		}
+		
+		if(cont != 0) {
+			System.out.println(maior);
+		}
+		
+		s.close();
+		
+
+	}
+
+}
