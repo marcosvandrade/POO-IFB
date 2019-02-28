64a9c52639f5d71a812f36324e06afcc88169a5c
Aluno <Aluno@admin-PC>
2019-02-28 11:01:43 -0300

    acidezSolucao

--- POO-IFB/src/thehuxley/acidezSolucao.java
+++ POO-IFB/src/thehuxley/acidezSolucao.java
@@ -1,8 +1,13 @@
 package thehuxley;
 
+import java.util.Scanner;
+
 public class acidezSolucao {
 
 	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		double valor = s.nextDouble();
 		
 		
 
