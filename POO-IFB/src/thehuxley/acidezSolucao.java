7e64ac30a8103279580b302bfa2d80689bfc3138
Marcos Vasconcellos de Andrade <dcemvasconcellos@DCEM-02182.dgp.local>
2019-03-01 11:52:56 -0300

    acidezSolucao almocoCantina

--- POO-IFB/src/thehuxley/acidezSolucao.java
+++ POO-IFB/src/thehuxley/acidezSolucao.java
@@ -7,10 +7,30 @@ public class acidezSolucao {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		double valor = s.nextDouble();
+		float valor = s.nextFloat();
+				
+		if((valor >= 1.0) && (valor <= 14.0)) {
 		
+			if(valor < 7) {
+				
+				System.out.println("Acida");
+				
+			}
+			
+			if(valor > 7) {
+				
+				System.out.println("Basica");
+				
+			}
+	
+			if(valor == 7) {
+				
+				System.out.println("Neutra");
+				
+			}
 		
-
+		}
+			s.close();	
 	}
 
 }
---
+++ POO-IFB/src/thehuxley/almocoCantina.java
@@ -0,0 +1,33 @@
+package thehuxley;
+
+import java.util.Scanner;
+
+public class almocoCantina {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		String comida = s.next();
+		String bebida = s.next();
+		
+		enum comida{
+			
+		}
+		
+		double totalComida;
+		double totalBebida;
+				
+		switch (comida.hashCode()) {
+		case String lasanha.hashCode():
+			
+			totalComida = 8.00;
+			
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+
+}
