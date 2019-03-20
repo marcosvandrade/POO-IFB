0d81cfa319a587bb3be568633ae67c3907fd473c
Aluno <Aluno@admin-PC>
2019-03-20 09:54:50 -0300

    atualizaIFB

---
+++ POO-IFB/src/exemploHeitorTurma/Principal.java
@@ -0,0 +1,45 @@
+package exemploHeitorTurma;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int opt=1;
+		Turma turma = new Turma("tsi32421", "POO", 20);
+		while(opt!= 0) {
+			System.out.println("Escolha uma opcao:");
+			System.out.println("1 -  adicionar aluno");
+			System.out.println("2 -  remover aluno");
+			System.out.println("3 -  listar alunos");
+			System.out.println("0 -  Encerrar programa");
+			opt = teclado.nextInt();
+			teclado.nextLine();
+			switch(opt) {
+			case 1:
+				System.out.println("Digite a matrícula");
+				String matricula =  teclado.nextLine();
+				System.out.println("Digite o nome");
+				String nome =  teclado.nextLine();
+				Aluno novoALuno = new Aluno(matricula, nome);
+				turma.adicionar(novoALuno);
+				break;
+			case 2:
+				System.out.println("Digite a matricula:");
+				String mat = teclado.nextLine();
+				turma.remover(mat);
+				break;
+			case 3:
+				turma.listar();
+				break;
+			}
+			
+		
+		}
+		
+		
+		
+		
+		
+	}
+}
--- POO-IFB/src/restaurante/Conta.java
+++ POO-IFB/src/restaurante/Conta.java
@@ -8,9 +8,9 @@ public abstract class Conta {
 		
 		//criando os produtos
 		Produto[] produtos = new Produto[20];
-		//refeições
-		produtos[0] = new Produto(1,"Filé de Frango",10.90);
-		produtos[1] = new Produto(2,"Filé de Angus",12.90);
+		//refeicoes
+		produtos[0] = new Produto(1,"File de Frango",10.90);
+		produtos[1] = new Produto(2,"File de Angus",12.90);
 		produtos[2] = new Produto(3,"Parmegiana",16.90);
 		produtos[3] = new Produto(4,"Picanha 120 gr",16.90);
 		produtos[4] = new Produto(5,"Picanha 240 gr",24.90);
@@ -18,10 +18,10 @@ public abstract class Conta {
 		produtos[5] = new Produto(6,"Refrigerante 600ml",4.50);
 		produtos[6] = new Produto(7,"Refrigerante 2 litros",7.50);
 		produtos[7] = new Produto(8,"Refrigerante lata",3.50);
-		produtos[8] = new Produto(9,"Água 500ml",2.50);
+		produtos[8] = new Produto(9,"Agua 500ml",2.50);
 		produtos[9] = new Produto(10,"Suco",4.50);
 		//sobremesas
-		produtos[10] = new Produto(11,"Torta de maça",18.00);
+		produtos[10] = new Produto(11,"Torta de maca",18.00);
 		produtos[11] = new Produto(12,"Salada de Frutas",15.00);
 		produtos[12] = new Produto(13,"Pudim de doce de leite",15.00);
 		produtos[13] = new Produto(14,"Cesta de Churros com sorvete de doce de leite",22.00);
